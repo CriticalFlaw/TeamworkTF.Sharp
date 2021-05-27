3b2a18adc1d12f97c019f1cf3246f0813204a9e2
CriticalFlaw <nik_101@live.co.uk>
2021-05-27 11:34:26 -0400

    Disabled Steam API tests
    - Set the Steam API tests to be ignored as calling them yield inconsistent results.

--- src/TeamworkTF.Test/Setup.cs
+++ src/TeamworkTF.Test/Setup.cs
@@ -1,21 +1,18 @@
 using System;
 using NUnit.Framework;
-using TeamworkTF.Sharp;
 
 namespace TeamworkTF.Test
 {
     public class Setup
     {
         public Sharp.TeamworkTF TeamworkTF;
-        public SteamApi Steam;
+        public Sharp.SteamApi Steam;
 
         [OneTimeSetUp]
         public void GetSetup()
         {
-            var teamworkTf = Environment.GetEnvironmentVariable("teamworkTf") ?? "";
-            var steam = Environment.GetEnvironmentVariable("steam") ?? "";
-            TeamworkTF = new Sharp.TeamworkTF(teamworkTf);
-            Steam = new SteamApi(Environment.GetEnvironmentVariable(steam));
+            TeamworkTF = new Sharp.TeamworkTF(Environment.GetEnvironmentVariable("teamworkTf") ?? "");
+            Steam = new Sharp.SteamApi(Environment.GetEnvironmentVariable(Environment.GetEnvironmentVariable("steam") ?? ""));
         }
     }
 }
